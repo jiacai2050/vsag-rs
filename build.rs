447d5871dfbda7ed40f020d859d7d08ac9558492
jiacai2050 <dev@liujiacai.net>
2024-11-08 14:08:09 +0800

    add submodule

--- build.rs
+++ build.rs
@@ -13,21 +13,21 @@
 // limitations under the License.
 
 fn main() {
-    println!("cargo:rerun-if-changed=include/wrapper.h");
-    println!("cargo:rerun-if-changed=src/wrapper.cpp");
+    // println!("cargo:rerun-if-changed=include/wrapper.h");
+    // println!("cargo:rerun-if-changed=src/wrapper.cpp");
     println!("cargo:rerun-if-changed=build.rs");
     println!("cargo:rustc-cdylib-link-arg=-fPIC");
 
-    let dst = cmake::Config::new("")
-        .build_target("vsag_wrapper")
+    let dst = cmake::Config::new("vsag-sys")
+        // .build_target("vsag_wrapper")
         // Cargo sets TARGET to the target triple
         // but building openblas via cmake will fail if it's set
         .env("TARGET", "")
         .build();
 
-    println!("cargo:rustc-link-lib=dylib=stdc++");
-    println!("cargo:rustc-link-lib=static=vsag_wrapper");
-    println!("cargo:rustc-link-lib=dylib=vsag");
+    // println!("cargo:rustc-link-lib=dylib=stdc++");
+    println!("cargo:rustc-link-lib=static=vsag_static");
+    // println!("cargo:rustc-link-lib=dylib=vsag");
     println!("cargo:rustc-link-search=native={}/build", dst.display());
     println!(
         "cargo:rustc-link-search=native={}/build/_deps/vsag-build/src",
