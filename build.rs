d46ea8d9ba89833b1a810411637ab13dcb088b92
jiacai2050 <dev@liujiacai.net>
2024-11-08 22:29:21 +0800

    bump version 0.1.0

--- build.rs
+++ build.rs
@@ -13,26 +13,18 @@
 // limitations under the License.
 
 fn main() {
-    // println!("cargo:rerun-if-changed=include/wrapper.h");
-    // println!("cargo:rerun-if-changed=src/wrapper.cpp");
     println!("cargo:rerun-if-changed=build.rs");
     println!("cargo:rustc-cdylib-link-arg=-fPIC");
 
     let dst = cmake::Config::new("vsag-sys")
-        // .very_verbose(true)
-        // .always_configure(true)
-        // .build_target("vsag_wrapper")
         // Cargo sets TARGET to the target triple
         // but building openblas via cmake will fail if it's set
+        // ```plaintext
+        // The TARGET specified on the command line or in Makefile.rule is not supported. Please choose a target from TargetList.txt
+        // ```
         .env("TARGET", "")
         .build();
 
-    // println!("cargo:rustc-link-lib=dylib=stdc++");
-    // println!("cargo:rustc-link-lib=dylib=vsag");
+    println!("cargo:rustc-link-lib=dylib=vsag");
     println!("cargo:rustc-link-search=native={}/lib", dst.display());
-    // println!(
-    //     "cargo:rustc-link-search=native={}/build/_deps/vsag-build/src",
-    //     dst.display()
-    // );
-    println!("cargo:rustc-link-lib=static=vsag_static");
 }
